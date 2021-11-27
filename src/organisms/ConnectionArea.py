aafda4a011441c04eaefd9735bbf411a4d645104
xcloudyunx <46149323+xcloudyunx@users.noreply.github.com>
2021-11-27 22:32:26 +1300

    prepping for plugin manager

--- src/organisms/ConnectionArea.py
+++ src/organisms/ConnectionArea.py
@@ -14,11 +14,24 @@ class ConnectionArea(wx.Panel):
 		sizer = wx.BoxSizer(wx.VERTICAL)
 		self.SetSizer(sizer)
 		
-		title = TextElement(parent=self, value="ConnectionArea")
-		sizer.Add(title, wx.SizerFlags(2).Expand())
+		# title = TextElement(parent=self, value="ConnectionArea")
+		# sizer.Add(title, wx.SizerFlags(2).Expand())
 		
-		manual = TextElement(parent=self, value=constants.IP)
-		sizer.Add(manual, wx.SizerFlags(2).Expand())
+		# add a sizer to make button one row
+		
+		pluginManagerButton = CustomButton(
+			parent=self,
+			value="Plugin Manager",
+			onClick=lambda evt : None
+		)
+		sizer.Add(pluginManagerButton, wx.SizerFlags(0).Centre())
+		
+		syncButton = CustomButton(
+			parent=self,
+			value="Sync",
+			onClick=lambda evt : onSyncButtonClick()
+		)
+		sizer.Add(syncButton, wx.SizerFlags(0).Centre())
 		
 		qr = qrcode.QRCode()
 		qr.add_data(constants.IP)
@@ -29,11 +42,9 @@ class ConnectionArea(wx.Panel):
 		auto = wx.StaticBitmap(parent=self, bitmap=bmp)
 		sizer.Add(auto, wx.SizerFlags(20).Expand())
 		
-		syncButton = CustomButton(
-			parent=self,
-			value="Sync",
-			onClick=lambda evt : onSyncButtonClick()
-		)
-		sizer.Add(syncButton, wx.SizerFlags(0).Centre())
+		# change text element? or maybe just this one
+		manual = TextElement(parent=self, value=constants.IP)
+		sizer.Add(manual, wx.SizerFlags(2).Expand())
+		
 		
 		sizer.Add(0, 0, wx.SizerFlags(1))
